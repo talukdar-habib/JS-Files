3cf4770d590788f4abfda695c2c4af33a9f45e75
talukdar-habib <habib291030@gmail.com>
2024-09-17 18:00:30 +0600

    slice.js added

---
+++ JS-String-Object/slice.js
@@ -0,0 +1,21 @@
+const country = 'Bangladesh';
+const part = country.slice(0,4);
+// console.log(part);
+// slice --> kono ekta pura jinish er kichu part alada kore neyar jonno
+
+
+const sentence = 'I am a good and hardworking person';
+// console.log(sentence.split('a'));   
+
+//console.log(sentence.split('')); --> shob gula word k letter e alada kore. console.log(sentence.split(' ')); --> majkhane erkm space thakle space chara only word gulake alada kore. r jodi kono letter ba word deya thake tahole oita diyeo vag kora jay
+
+
+const friends = 'Habib,Labib,Khabib,Mahib,Sabib';
+console.log(friends.split(','));
+
+
+const district = ['Dhaka', 'Rajshahi', 'Khulna', 'Sylhet'] ;
+console.log(district);
+console.log(district.join('-')); //elements join korar jonno join() method use kora hoy
+
+
