50ba32e24f149e38eff3fc6c4cb53cc1afdeb091
talukdar-habib <habib291030@gmail.com>
2024-09-18 11:10:12 +0600

    object.js added

---
+++ JS-String-Object/object.js
@@ -0,0 +1,36 @@
+// Primitive
+// Basic-primary
+
+const age = 25;
+const name = 'Habib';
+const isPassed = true;
+let isDeveloper;
+// console.log(isDeveloper);
+
+const subjects = ['bangla', 'english', 'math', 'physics'];
+const shirt = ['half-shirt', 54, 'full'];
+const shirtType = 'Half-Shirt';
+const shirtSize = 155;
+const shirtColor = 'red';
+
+
+
+// Object
+// Non primitive
+
+const subject = {
+    name : 'bangla',
+    role : 'teacher',
+    salary : 20000,
+    isClassTeacher: false,
+
+}
+
+// Non primitive
+
+const people = {
+    name : 'Habib',
+    age : 25,
+    position : 'Web Developer',
+    isSkilled : true,
+}
