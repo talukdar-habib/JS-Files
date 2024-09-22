0ded0ebe4c4d97ecd1c498133bbabecdf35ea338
talukdar-habib <habib291030@gmail.com>
2024-09-23 00:52:49 +0600

    declare.js added

---
+++ JS-String-Object/declare.js
@@ -0,0 +1,9 @@
+// evabe object declare kora jay
+
+const pen = { brand : 'apple', price : 10000, color : 'black'}
+const pencil = new Object()
+// pencil.firstName = 'habib';
+// pencil.lastName = 'talukdar';
+console.log(pencil);
+const rubber = Object.create({})
+console.log(rubber);
