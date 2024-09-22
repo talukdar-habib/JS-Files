adaa3c48161d910d9297bd3abb8b4c248f6a1966
talukdar-habib <habib291030@gmail.com>
2024-09-22 23:34:04 +0600

    keys.js and values.js added

---
+++ JS-String-Object/keys.js
@@ -0,0 +1,10 @@
+const computer = {
+    brand : 'Dell',
+    price : 40000,
+    processor : 'intel',
+    hdd : '1tb',
+    monitor : 'samsung'
+}
+
+const keys = Object.keys(computer); //property/keys gula eksathe access kora jay 
+console.log(keys);
---
+++ JS-String-Object/values.js
@@ -0,0 +1,10 @@
+const computer = {
+    brand : 'Dell',
+    price : 40000,
+    processor : 'intel',
+    hdd : '1tb',
+    monitor : 'samsung'
+}
+
+const values = Object.values(computer); //elements/values gula eksathe access kora jay
+console.log(values);
