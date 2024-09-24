3d9e69a2f651044bb3f7e9f45a9b54a55c2e6916
talukdar-habib <habib291030@gmail.com>
2024-09-24 06:51:52 +0600

    added

--- JS-Array-More/reverse.js
+++ JS-Array-More/reverse.js
@@ -28,10 +28,20 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // reverse  side
 
-const revRev = [];
-for(let i = numbers.length -1; i >= 0;  i--){
-    const num = numbers[i];
-    console.log(num);
-    revRev.push(num);
+// const revRev = [];
+// for(let i = numbers.length -1; i >= 0;  i--){
+//     const num = numbers[i];
+//     console.log(num);
+//     revRev.push(num);
+// }
+// console.log(revRev);
+
+
+const char = ['a', 'b', 'c', 'd', 'e', 'f'];
+const letter = [];
+
+for(const pep of char){
+    console.log(pep);
+    letter.unshift(pep);
 }
-console.log(revRev);
+console.log(letter);
