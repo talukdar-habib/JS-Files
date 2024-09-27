b8c4c50a61d9c2afaa235ec2d44ebb0061775859
talukdar-habib <habib291030@gmail.com>
2024-09-27 22:40:46 +0600

    parameter.js added

---
+++ JS-Function/function.js
@@ -0,0 +1,21 @@
+
+const price = 100;
+
+// function evabe declare korte hoy
+// function name(){
+// }
+
+function cricket(){
+    console.log('Buy a bat');
+    console.log('Buy a bowl');
+    console.log('Buy a stump');
+}
+// call the function for output
+cricket();
+
+function webDevelop(){
+    console.log('first e html');
+    console.log('then css');
+    console.log('Javascript');
+}
+webDevelop();
---
+++ JS-Function/parameter.js
@@ -0,0 +1,28 @@
+
+
+// function answer(number){
+//     console.log(number);
+//     const square = number * number;
+//     console.log('Square is :', square);
+// }
+// answer(10);
+// console.log('.................');
+// answer(20);
+// answer(50);
+
+
+// function add(num1,num2){
+//     const sum = num1+num2;
+//     console.log('Sum is:',sum);
+// }
+// add(10,50);
+// add(100,300);
+
+
+function jog(a,b,c,d,e){
+    const total = a+b+c+d+e;
+    // console.log(a,b,c,d,e);
+    console.log('Summation is:',total);
+}
+jog(1,10,30,40,50);
+jog(1,10,30,40);  // ekhane joto gula variable declare korbo totogula value na dile NaN(not a number) show korbe
