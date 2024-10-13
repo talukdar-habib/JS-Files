76a9ab17b7931941ff1c989f1a3c17dfd832df8e
talukdar-habib <habib291030@gmail.com>
2024-10-13 19:44:56 +0600

    math.js file added

---
+++ JS-Problems-Part-1/math.js
@@ -0,0 +1,25 @@
+
+
+const min = Math.min (10,25,23,65,25,6); // koyekta shokhar moddhe jeta shob cheye choto oita evabe ber kora jay
+console.log('Mimimum number is: ', min);
+const max = Math.max (10,25,23,65,25,6); //koyekta shokhar moddhe jeta shob cheye boro oita evabe ber kora jay
+console.log('Maximum number is: ', max);
+
+console.log('Value of PI is: ', Math.PI); // PI er man ber kora hoy evabe
+
+console.log('Absolute value is: ', Math.abs(-55)); // sign ta ke bad diye absolute man ta k ney
+console.log('Gap is: ', Math.abs(5-13)); // shonkhar moddhe gap ber kora hoy evabe
+
+console.log(Math.round(4.89)); // purno shongkha ber kore dibe r jei number ta shob theke kache thakbe oita diye round kore dibe
+console.log(Math.round(4.25));
+
+console.log(Math.floor(5.69)); // floor diye niche namabe ba nicher dike j purno shonkha ase oita dekhabe ex: 5.69= 5
+
+console.log(Math.ceil(6.24)); //ceil diye upore tulbe ba uporer dike j purno shonkha ase oita dekhabe ex: 6.24= 7
+
+console.log(Math.random());// random diye 0 the 9 porjonto jekno number er fraction ba doshomik shonkha dekhabe
+
+console.log(Math.random()*10); //random diye 0 the 9 porjonto jekno number er sathe 10 gun kore fraction ba doshomik shonkha dekhabe
+
+const rand = Math.round(Math.random()*10); // eta te random shonkhar 10 diye gun kore purno shonkha dekhabe
+console.log(rand);
